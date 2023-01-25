a422f023b629878e9dd7048ae2332c0ce4888b31
j3m3 <y.j3ms.n@gmail.com>
2023-01-26 08:19:45 +0900

    refactor: merge TLE codes of 1874 with AC codes

--- Python/1874_스택수열.py
+++ Python/1874_스택수열.py
@@ -21,3 +21,34 @@ for num in nums:
 
 else:
     print('/n'.join(commands))
+
+# ----------------------------
+
+import sys
+
+input()
+nums = list(map(int, sys.stdin.read().split()))
+commands = []
+stack = []
+start = 0
+
+for num in nums:
+    if num in stack: # in의 시간 복잡도가 O(n)이기 때문에 TLE
+        if num == stack[-1]:
+            stack.pop()
+            commands.append("-")
+        else:
+            print("NO")
+            break
+    else:
+        for i in range(start+1, num+1):
+            stack.append(i)
+            commands.append("+")
+
+        stack.pop()
+        commands.append("-")
+        
+        start = num
+else:
+    for c in commands:
+        print(c)
