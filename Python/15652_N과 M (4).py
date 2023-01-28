d294dcf7d4988ebc77324722c8751dd466c259a1
j3m3 <y.j3ms.n@gmail.com>
2023-01-28 17:39:19 +0900

    feat: use back tracking to solve

---
+++ Python/15652_N과 M (4).py
@@ -0,0 +1,15 @@
+input = __import__("sys").stdin.readline
+
+n, r = map(int, input().split())
+
+def dfs(arr, start):
+    if len(arr) == r:
+        print(*arr)
+        return
+
+    for i in range(start, n+1):
+        arr.append(i)
+        dfs(arr, i)
+        arr.pop()
+
+dfs([], 1)
