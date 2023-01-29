246d232b03417dc3786183125ecf195b2283e2a0
j3m3 <y.j3ms.n@gmail.com>
2023-01-29 21:06:29 +0900

    refactor: add a solution optimized by using indices

--- Python/1018_체스판 다시 칠하기.py
+++ Python/1018_체스판 다시 칠하기.py
@@ -23,3 +23,34 @@ for i in range(ncol - len(chess) + 1):
         case_2.append(cnt_2)
 
 print(min(min(case_1), min(case_2)))
+
+# -----------------------------------
+
+input = __import__("sys").stdin.readline
+N, M = map(int, input().split())
+
+board = [input().rstrip() for _ in range(N)]
+sample_1 = ['W', 'B']
+sample_2 = ['B', 'W']
+
+min_count = float('inf')
+for y in range(N-7):
+    for x in range(M-7):
+        count_1 = 0
+        count_2 = 0
+        for i in range(y, y+8):
+            for j in range(x, x+8):
+                s = board[i][j]
+
+                if s == sample_1[j & 1]:
+                    count_1 += 1
+                elif s == sample_2[j & 1]:
+                    count_2 += 1
+
+            sample_1.reverse()
+            sample_2.reverse()
+
+        if min_count > (count := min(count_1, count_2)):
+            min_count = count
+
+print(min_count)
