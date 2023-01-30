ed1ffc86dfe0d9288d657c79cd5a995eca81c516
j3m3 <y.j3ms.n@gmail.com>
2023-01-30 20:05:05 +0900

    refactor: delete visited array to optimize

--- Python/15650_N과 M (2).py
+++ Python/15650_N과 M (2).py
@@ -1,7 +1,6 @@
 input = __import__("sys").stdin.readline
 
 n, r = map(int, input().split())
-visited = [False] * (n+1)
 
 def dfs(arr, start):
     if len(arr) == r:
@@ -9,11 +8,8 @@ def dfs(arr, start):
         return
 
     for i in range(start, n+1):
-        if not visited[i]:
-            visited[i] = True
-            arr.append(i)
-            dfs(arr, i+1)
-            arr.pop()
-            visited[i] = False
+        arr.append(i)
+        dfs(arr, i+1)
+        arr.pop()
 
 dfs([], 1)
