ae57b916595cd6113b538ba2c7e0533f791c6552
j3m3 <y.j3ms.n@gmail.com>
2023-01-31 22:59:23 +0900

    feat: add a solution without loop in IsPromising

--- C++/9663_N-Queen.cpp
+++ C++/9663_N-Queen.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define MAX 15
 
 int N;
@@ -39,4 +39,48 @@ int main() {
     cout << count;
 
     return 0;
+}
+
+// ------------------------------------
+
+#include <iostream>
+
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define MAX 15
+
+int N;
+int count = 0;
+bool col_visited[MAX*2-1];
+bool sum_rowcol[MAX*2-1]; // Check diag shape like '/'
+bool sub_rowcol[MAX*2-1]; // Check diag shape like '\'
+
+void DFS(int r) {
+    if(r == N) {
+        count++;
+        return;
+    }
+    for(int c = 0; c < N; c++) {
+        if(col_visited[c] || sum_rowcol[c+r] || sub_rowcol[r-c+N-1])
+            continue;
+
+        col_visited[c] = true;
+        sum_rowcol[c+r] = true;
+        sub_rowcol[r-c+N-1] = true;
+
+        DFS(r + 1);
+
+        col_visited[c] = false; 
+        sum_rowcol[c+r] = false;
+        sub_rowcol[r-c+N-1] = false;
+    }
+}
+
+
+int main() {
+    fastio;
+
+    cin >> N;
+    DFS(0);
+    cout << count;
 }
