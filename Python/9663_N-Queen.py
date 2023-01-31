4463b27e3a65d31b6d6e233af06220f24515c8e4
j3m3 <y.j3ms.n@gmail.com>
2023-01-31 23:22:30 +0900

    feat: add a solution without loop in is_diag

--- Python/9663_N-Queen.py
+++ Python/9663_N-Queen.py
@@ -27,3 +27,36 @@ def dfs(arr):
 count = 0
 dfs([])
 print(count)
+
+# ------------------------------------
+
+input = __import__("sys").stdin.readline
+
+N = int(input())
+count = 0
+col_visited = [False] * (2*N+1)
+sum_rowcol_map = [False] * (2*N+1)
+sub_rowcol_map = [False] * (2*N+1)
+
+def DFS(r):
+    if r == N:
+        global count
+        count += 1
+        return
+    
+    for c in range(N):
+        if col_visited[c] or sum_rowcol_map[r+c] or sub_rowcol_map[r-c+N-1]:
+            continue
+
+        col_visited[c] = True
+        sum_rowcol_map[r+c] = True
+        sub_rowcol_map[r-c+N-1] = True
+
+        DFS(r+1)
+
+        col_visited[c] = False
+        sum_rowcol_map[r+c] = False
+        sub_rowcol_map[r-c+N-1] = False
+
+DFS(0)
+print(count)
