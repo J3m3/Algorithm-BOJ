2ff88c6732eb240c5ac3cdec81f651c0755f4ab6
j3m3 <y.j3ms.n@gmail.com>
2023-02-03 00:08:03 +0900

    feat: add a solution with visited array

--- Python/14889_스타트와 링크.py
+++ Python/14889_스타트와 링크.py
@@ -39,3 +39,47 @@ def dfs(team: set, start):
 
 dfs(set(), 0)
 print(min_diff)
+
+# --------------------------
+
+input = __import__("sys").stdin.readline
+
+N = int(input())
+S = [list(map(int, input().split())) for _ in range(N)]
+min_diff = float('inf')
+visited = [False] * N
+
+
+def calculate_ability_diff():
+    team_link = 0
+    team_start = 0
+
+    # Using 'in' with set is much more slower than accessing through indicies
+    # Also, nested for loop for each team is slightly faster!
+    # nested for loop for each team: 10^2 + 10^2 = 200 when N == 20
+    # nested for loop like below (just one nested for loop): (20 * 21) / 2 = 210 when N == 20
+    for i in range(N):
+        for j in range(i+1, N):
+            if visited[i] and visited[j]:
+                team_link += S[i][j] + S[j][i]
+
+            elif not visited[i] and not visited[j]:
+                team_start += S[i][j] + S[j][i]
+
+    return abs(team_link - team_start)
+
+
+def dfs(depth, start):
+    if depth == N // 2:
+        diff = calculate_ability_diff()
+        global min_diff
+        min_diff = min(min_diff, diff)
+        return
+    
+    for i in range(start, N):
+        visited[i] = True
+        dfs(depth+1, i+1)
+        visited[i] = False
+
+dfs(0, 0)
+print(min_diff)
