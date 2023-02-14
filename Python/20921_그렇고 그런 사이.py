ebfa1166d2c180ae27c931b8e138673e20800060
j3m3 <y.j3ms.n@gmail.com>
2023-02-15 00:42:36 +0900

    feat: use an extra array to reduce time complexity

--- Python/20921_그렇고 그런 사이.py
+++ Python/20921_그렇고 그런 사이.py
@@ -15,3 +15,26 @@ def count_some(arr, target):
 N, K = map(int, input().split())
 arr = [i for i in range(1, N+1)]
 print(*count_some(arr, K))
+
+# -----------------------------------------
+
+input = __import__("sys").stdin.readline
+
+N, K = map(int, input().split())
+arr = [i for i in range(1, N+1)]
+head = []
+
+n = N - 1
+while K:
+    if K >= n:
+        K -= n
+        head.append(n + 1)
+        arr[n] = 0
+    
+    n -= 1
+
+for n in head:
+    print(n, end=" ")
+for n in arr:
+    if n:
+        print(n, end=" ")
