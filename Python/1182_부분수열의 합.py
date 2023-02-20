8ea1b00f0ddb9f1dad6d6f5fb61373ebf5534a1b
j3m3 <y.j3ms.n@gmail.com>
2023-02-20 18:51:40 +0900

    feat: add a solution using recursion

--- Python/1182_부분수열의 합.py
+++ Python/1182_부분수열의 합.py
@@ -31,3 +31,23 @@ for i in range(1, 1 << N):
         count += 1
 
 print(count)
+
+# -------------------------------------------
+
+input = __import__("sys").stdin.readline
+
+def count_target(depth, total):
+    if depth == N:
+        if total == target:
+            global count
+            count += 1
+        return
+    
+    count_target(depth+1, total + arr[depth])
+    count_target(depth+1, total)
+
+N, target = map(int, input().split())
+arr = list(map(int, input().split()))
+count = 0
+count_target(0, 0)
+print(count if target else count - 1)
