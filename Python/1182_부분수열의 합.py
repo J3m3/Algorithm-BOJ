96051c961a0b99c3c356a41bacfeedaff5012213
j3m3 <y.j3ms.n@gmail.com>
2023-02-20 17:28:45 +0900

    feat: add a solution without built-in sum func (much slower)

--- Python/1182_부분수열의 합.py
+++ Python/1182_부분수열의 합.py
@@ -11,3 +11,23 @@ for i in range(1 << N):
         count += 1
 
 print(count)
+
+# -------------------------------------------
+
+input = __import__("sys").stdin.readline
+
+N, target = map(int, input().split())
+arr = list(map(int, input().split()))
+
+masks = [1 << i for i in range(N)]
+count = 0
+for i in range(1, 1 << N):
+    total = 0
+    for n in range(N):
+        if masks[n] & i:
+            total += arr[n]
+
+    if total == target:
+        count += 1
+
+print(count)
