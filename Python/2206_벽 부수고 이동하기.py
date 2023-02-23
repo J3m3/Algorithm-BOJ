605beaf3869f5e14c2f6cd3cabbd5de498c83fe3
j3m3 <y.j3ms.n@gmail.com>
2023-02-23 22:50:05 +0900

    feat: add a solution with 3D visited array (more readable)

--- Python/2206_벽 부수고 이동하기.py
+++ Python/2206_벽 부수고 이동하기.py
@@ -48,3 +48,47 @@ while queue:
             w = 0
 else:
     print(-1)
+
+# -----------------------------------------------------
+
+from collections import deque
+input = __import__("sys").stdin.readline
+
+def bfs():
+    queue = deque([(0, 0, 0)])
+    visited[0][0][0] = 1
+
+    while queue:
+        used_wall, y, x = queue.popleft()
+        if x == M - 1 and y == N - 1:
+            return visited[used_wall][y][x]
+        
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if not (0 <= nx < M and 0 <= ny < N):
+                continue
+
+            # 정확히 한 번만 벽을 부수고 이동할 수 있기 때문에, 방문 여부 신경 쓰지 않아도 됨
+            if board[ny][nx] == WALL and not used_wall:
+                visited[WALL][ny][nx] = visited[PATH][y][x] + 1
+                queue.append((WALL, ny, nx))
+                
+            elif board[ny][nx] == PATH and not visited[used_wall][ny][nx]:
+                visited[used_wall][ny][nx] = visited[used_wall][y][x] + 1
+                queue.append((used_wall, ny, nx))
+    
+    return -1
+
+
+PATH = 0
+WALL = 1
+N, M = map(int, input().split())
+board = [list(map(int, input().rstrip())) for _ in range(N)]
+visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(2)]
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+print(bfs())
