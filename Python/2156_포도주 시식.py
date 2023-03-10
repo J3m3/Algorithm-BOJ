dcebf2164f0741cc13a863af63822d6dc36cdf5f
j3m3 <y.j3ms.n@gmail.com>
2023-03-10 17:27:25 +0900

    feat: add a more understandable solution

--- Python/2156_포도주 시식.py
+++ Python/2156_포도주 시식.py
@@ -18,3 +18,30 @@ for n in range(3, N+1):
     )
 
 print(dp[N])
+
+# ----------------------------------
+
+input = __import__("sys").stdin.readline
+
+N = int(input())
+arr = [0] * 10002
+for i in range(N):
+    arr[i] = int(input())
+
+dp = [0] * 10002
+dp[0] = arr[0]
+dp[1] = dp[0] + arr[1]
+dp[2] = max(
+    arr[0] + arr[2], 
+    arr[2] + arr[1], 
+    dp[1]
+)
+
+for n in range(3, N):
+    dp[n] = max(
+        dp[n-2] + arr[n],
+        dp[n-3] + arr[n] + arr[n-1],
+        dp[n-1]
+    )
+
+print(dp[N-1])
