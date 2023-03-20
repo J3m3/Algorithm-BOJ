deec6ad256923e0eb0d541f24aaf130c0a4eb822
j3m3 <y.j3ms.n@gmail.com>
2023-03-20 23:32:49 +0900

    feat: add a solution with undirected graph (Floyd-Warshall)

--- C++/2458_키 순서.cpp
+++ C++/2458_키 순서.cpp
@@ -45,3 +45,48 @@ int main() {
 
     return 0;
 }
+
+// --------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int N, M, cnt;
+int graph[502][502];
+int check[502];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u][v] = 1;
+        check[u]++;
+        check[v]++;
+    }
+
+    for (int k = 1; k <= N; k++)
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= N; j++) {
+                if (graph[i][j])
+                    continue;
+                if (graph[i][k] == 1 && graph[k][j] == 1) {
+                    graph[i][j] = 1;
+                    check[i]++;
+                    check[j]++;
+                }
+            }
+
+    for (int i = 1; i <= N; i++)
+        if (check[i] == N - 1)
+            cnt++;
+
+    cout << cnt;
+
+    return 0;
+}
